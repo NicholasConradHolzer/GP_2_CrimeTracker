8285560a0f80d922cedbd54ac0cecd2189329f67
pmnni87@gmail.com <91038339+pmanning901@users.noreply.github.com>
2022-01-28 16:36:08 -0600

    new functions Chart.JS

--- charts.js
+++ charts.js
@@ -1,12 +1,17 @@
 //Fetch Data from REST Crime API
 
 async function getCrimeData() {
-const apiUrl = ""
-const response = await fetch(apiUrl)
-const barChartData = await response.json()
+    const apiUrl = ""
+    const response = await fetch(apiUrl)
+    const barChartData = await response.json()
 
-console.log(barChartData)
+    const crimeType = barChartData.map(  (x) => x.crime_committed)
+    const dateCrime = barChartData.map((x) => x.comitted_date)
+    
 
 
-
+    console.log(barChartData)
 }
+getCrimeData()
+
+
