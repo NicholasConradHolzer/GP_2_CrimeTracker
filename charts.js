39a0e6d46a01eb1806a7c4437f6da771682187df
pmnni87@gmail.com <91038339+pmanning901@users.noreply.github.com>
2022-01-30 23:21:12 -0600

    new functions for Charts

--- charts.js
+++ charts.js
@@ -1,4 +1,5 @@
 //Fetch Data from REST Crime API
+https://cdn.jsdelivr.net/npm/chart.js@3.7.0/dist/chart.min.js
 
 var ctx = document.getElementById('canvas').getContext('2d');
 var myChart = new Chart (ctx, {
@@ -9,7 +10,23 @@ var myChart = new Chart (ctx, {
     }
 
 })
-async function getCrimeData() {
+function getCrimeData() {
+    axios
+    .get('https://api.crimeometer.com/v2/incidents/stats?lat=33.729610&lon=-84.409736&distance=.25mi&datetime_ini=2020-01-01T00:00:00.000Z&datetime_end=2020-12-31T23:59:59.000Z')
+    .then((res) => {
+        //Hide chart error element on successful request
+        document.querySelector('#chart-error').classList.add('hidden');
+        cT = res.data.crime_incedent_type.toFixed(00);
+        nI = res.data.number_incedent_type.toFixed(00);
+//call updatechart() funtion to update data
+updateChart(robbery, dui, assault, drugO, blurglary, motortheft, )
+
+    })
+    .catch((e) =>
+      // Reveal API error element on failed request
+      document.querySelector('#chart-error').classList.remove('hidden')
+    );
+}
     const apiUrl = "https://api.crimeometer.com/v2/incidents/stats?lat=33.729610&lon=-84.409736&distance=.25mi&datetime_ini=2020-01-01T00:00:00.000Z&datetime_end=2020-12-31T23:59:59.000Z"
     const response = await fetch(apiUrl)
     const lineChartData = await response.json()
